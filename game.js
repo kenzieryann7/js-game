1b45a39d4bcc6fda3a13c20b3df1a28195c50a21
Makenzie <makenzie_99@hotmail.com>
2020-01-03 14:32:59 -0800

    implemented buying armor and gold capacity

--- game.js
+++ game.js
@@ -1,1 +1,95 @@
-<button type="button" class="btn btn-primary">Primary</button>
+let gold = {
+    amount: 0
+};
+//Increase Gold (Used to upgrade your character, building, and to buy items) 
+function increaseGold(amount) {
+    var i = document.getElementById('gold').innerHTML;
+    console.log("Calling increaseGold");
+      
+    i = parseInt(document.getElementById('gold').innerHTML);
+
+    if(i < 10) {
+        document.getElementById('gold').innerHTML = i+amount;
+    } 
+    if (i >= 10) {
+        document.getElementById('gold').disabled=true;
+    }
+      
+    console.log("Increased gold by 5");
+      
+}
+// Decrease Gold
+function decreaseGold(amount) {
+    var i = document.getElementById('gold').innerHTML;
+    console.log("Calling decreaseGold");
+      
+    i = parseInt(document.getElementById('gold').innerHTML);
+    
+    if (i >= 5) {
+        document.getElementById('gold').innerHTML = i-amount;
+    }
+    else {
+        alert("You do not have enough gold!")
+    }
+    console.log("Decreased gold by 5");
+      
+}
+
+// ELIXIR (Used to upgrade spells, research, etc) 
+function increaseElixir(amount) {
+    var i = document.getElementById('elixir').innerHTML;
+    console.log("Calling increaseElixir");
+    i = parseInt(document.getElementById('elixir').innerHTML);
+
+    document.getElementById('elixir').innerHTML = i+amount;
+      
+    console.log("Increased elixir by 5");
+      
+}
+
+
+// COMBO BUTTON 
+function combo(amount) {
+  var i = document.getElementById('combo').innerHTML;
+  console.log("Calling combo");
+  i = parseInt(document.getElementById('combo').innerHTML);
+
+  document.getElementById('combo').innerHTML = i+amount;
+      
+  console.log("Increased gold and elixir by 10");
+      
+}
+
+// BUY
+function buyArmor(amount) {
+    var i = document.getElementById('buy').innerHTML;
+    console.log("Calling buy function");
+      
+    i = parseInt(document.getElementById('buy').innerHTML);
+
+    if(i < 10) {
+        document.getElementById('buy').innerHTML = i-amount;
+    } 
+    if (i >= 10) {
+        document.getElementById('buy').disabled=true;
+    }
+      
+    console.log("Bought armor for 5 gold");
+    
+}
+
+// INCREASE GOLD CAPACITY
+function increaseGoldCap(amount) {
+    var currentAmount = document.getElementById('goldCap').innerHTML;
+    console.log("Calling goldCap function");
+    
+    currentAmount = parseInt(document.getElementById('goldCap').innerHTML);
+    
+    if (i === amount) {
+        document.getElementById('gold').innerHTML = currentAmount-amount;
+        document.getElementById('goldCap').innerHTML = i*2;
+    }
+      
+    console.log("Increase gold capacity");
+    
+}
