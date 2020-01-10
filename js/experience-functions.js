164e91e61e04ac2965031ff0a77cf00d33910786
Makenzie <makenzie_99@hotmail.com>
2020-01-10 11:45:07 -0800

    fixing problem with negative xp

--- js/experience-functions.js
+++ js/experience-functions.js
@@ -1,14 +1,55 @@
+const capBase = 100;
+
 function getCharacterLevel(amount) {
     console.log("Calling getCharacterLevel");
-    
-    let level = (amount+1)/100;
 
-    level = Math.ceil(level);
-    
+    let level = 1;
+    let cap = capBase;
+
+    while (amount >= cap) {
+        level++;
+        amount -= cap;
+        cap = newCap(cap);
+    }
     console.log("Level:", level);
 
     return level;
       
+}
+
+function getCap(amount) {
+    console.log("Calling getCharacterLevel");
+    let cap = capBase;
+
+    while (amount >= cap) {
+        amount -= cap;
+        cap = newCap(cap);
+    }
+
+    return cap;
+      
+}
+
+function getCapFloor(level) { //log
+    console.log("Calling getCharacterLevel");
+    let cap = capBase;
+
+    if(level == 0 ) {
+        cap = 0;
+    }
+
+    for(let i = 1; i < level; i += 1 ) {
+        cap += newCap(cap);
+    }
+    
+    return cap;
+      
+}
+
+function newCap(cap) {
+    cap = Math.pow(cap, 1.16);
+    cap = Math.round(cap);
+    return cap;
 }
 
 function increaseExperience(amount) {
@@ -21,10 +62,15 @@ function renderExperience() {
 
     console.log("Called renderExperience");
     const level = getCharacterLevel(experience.amount);
+    console.log("XP:", experience.amount);
+    console.log("XP cap:", experience.cap);
 
     
-
+    let current = experience.amount - getCapFloor(level - 1);
 
 
     experience.characterLevelElement.innerHTML = level;
+    experience.currentElement.innerHTML = current;
+    experience.capElement.innerHTML = getCap(experience.amount);
+    
 }
