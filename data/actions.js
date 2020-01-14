2d5bff062c98c5c9622542a1966e0bd8929c2bc3
Makenzie <makenzie_99@hotmail.com>
2020-01-14 14:10:29 -0800

    Got the action bar to say what action is currently running

--- data/actions.js
+++ data/actions.js
@@ -8,19 +8,31 @@ let combatAction = {};
 quarryingAction.run = () => {
     increaseStone(5);
     increaseExperience(3);
+    document.getElementById("actionType").innerHTML = quarryingAction.name;
 };
+
+quarryingAction.name = "Quarrying";
 
 woodcuttingAction.run = () => {
     increaseWood(5);
     increaseExperience(3);
+    document.getElementById("actionType").innerHTML = woodcuttingAction.name;
 };
+
+woodcuttingAction.name = "Woodcutting";
 
 miningAction.run = () => {
     increaseOre(5);
     increaseExperience(3);
+    document.getElementById("actionType").innerHTML = miningAction.name;
 };
+
+miningAction.name = "Mining";
 
 combatAction.run = () => {
     increaseGold(5);
     increaseExperience(3);
+    document.getElementById("actionType").innerHTML = combatAction.name;
 };
+
+combatAction.name = "Combat";
